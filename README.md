5c1b9a36c586bc0520c61286a6df68b46d9cbbb8
phryszlo <phryszlo@gmail.com>
2022-09-01 14:24:51 -0400

    this seems to be working

---
+++ README.md
@@ -0,0 +1,20 @@
+Objective: We are going to create an object Radio! 
+
+ 
+
+Happy coding!
+
+How to begin: 
+Create a new HTML/CSS/JS and complete the following in your .js file
+Make a radio Object.Give it a property called stations and make the value an array of station objects
+Give each station object a property called name and songs. Name should be a string and should be an array of song objects.
+Song object should have two properties: title and artist
+Create a method on your radio object that changes the station randomly. 
+When the station is changed, pick a song from that station console.log("Now Playing:" + song,title + "by" + song.artist)
+ 
+
+Submission: Submit a share link of your repl file here! 
+
+ 
+
+Happy coding!
---
+++ app.js
@@ -0,0 +1,56 @@
+
+const radio = {
+  stations: [
+    {
+      name: 'WOOO',
+      songs: [
+        {
+          title: "Charlie Don't Surf",
+          artist: 'The Clash'
+        },
+        {
+          title: 'Unwind',
+          artist: 'Sonic Youth'
+        }
+      ]
+    },
+    {
+      name: 'WGUC',
+      songs: [
+        {
+          title: 'Nocturne Op. 72. No. 1',
+          artist: 'F. Chopin'
+        },
+        {
+          title: 'Concerto Grosso Op. 3 No. 2',
+          artist: 'Antonio Vivaldi'
+        }
+      ]
+    },
+    {
+      name: 'WHFS',
+      songs: [
+        {
+          title: 'Tommy The Cat',
+          artist: 'Primus'
+        },
+        {
+          title: "What's The Frequency, Kenneth?",
+          artist: 'REM'
+        }
+      ]
+    }
+  ],
+
+  randomStation() {
+    const stationLength = radio.stations.length
+    let songsLength = 0;
+
+    const station = radio.stations[Math.floor(Math.random() * (radio.stations.length))];
+    const song = station.songs[Math.floor(Math.random() * (station.songs.length))];
+
+    console.log(`${station.name} now playing: ${song.title} by ${song.artist}`)
+  }
+}
+
+radio.randomStation();
